0780fd6da31e86f6962cf8a14f42c87440fd7512
YuFat <yunitafatmasari32@gmail.com>
2023-06-10 20:34:42 +0700

    first commit

---
+++ admin_depan.php
@@ -0,0 +1,9 @@
+<?php
+include("inc_header.php");
+?>
+<h1>Halaman Depan</h1>
+Selamat Datang di Halaman Depan
+<?php
+include("inc_footer.php");
+?>
+
---
+++ data_nilai.php
@@ -0,0 +1,9 @@
+<?php
+include("inc_header.php");
+?>
+<h1>Halaman nilai</h1>
+Selamat Datang di Halaman nilai
+<?php
+include("inc_footer.php");
+?>
+
